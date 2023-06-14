488388e7d6aecc3ac9522c71687a12cab2c231fc
Shubh-Personal <shubhpatel4799@gmail.com>
2023-06-14 15:02:11 -0400

    Changed to column chart

--- lambda_function.py
+++ lambda_function.py
@@ -82,7 +82,7 @@ class AwsDailCostAnalysis():
         worksheet.write_column('A2', services)
         worksheet.write_column('B2', cost)
         #selecting chart type
-        chart = workbook.add_chart({'type': 'bar'})
+        chart = workbook.add_chart({'type': 'column'})
         #Adding data in chart
         chart.add_series({
         'categories': '=Sheet1!$A$2:$A$7',
